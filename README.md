148cba0f05f84a06d287022a315c78e0e9a8637e
Tazri <mdtazri@gmail.com>
2022-08-09 02:07:41 +0600

    add README.md file and add favicon in site

---
+++ README.md
@@ -0,0 +1,11 @@
+[![Github](./asset/badge/github.svg)](https://github.com/Tazri) [![codepen](./asset/badge/condepen.svg)](https://codepen.io/md-tazri)
+
+Simple Portfolio Site 
+=====================
+
+This is simple portfolio site inspare by [EGATOR REACT PROJECT](https://youtu.be/G-Cr00UYokU). But here some difference. I create this site without react. I add light theme and theme changer switch and remove testimonial and project Components. Use sass for css. 
+
+Site Link
+----------
+- [Github Live Link](https://tazri.github.io/simple_portfolio_site_for_my_collage_cw/)
+- [Codpen Live Link](https://codepen.io/md-tazri/full/zYWaLod)
--- js/index.script.js
+++ js/index.script.js
@@ -4,16 +4,20 @@ let theme = document.getElementById('theme');
 let moon = document.getElementById('moon');
 let sun = document.getElementById('sun');
 let nav = document.getElementById('nav');
+let faviconlink = document.getElementById("favicon");
+console.log(faviconlink.href);
 
 
 moon.addEventListener('click',()=>{
     theme.className = 'icon themeicon sunshow';
     body.className = 'light';
+    faviconlink.href = './asset/favicon/light.png';
 })
 
 sun.addEventListener('click',()=>{
     theme.className = 'icon themeicon moonshow';
     body.className = 'dark';
+    faviconlink.href = './asset/favicon/dark.png';
 })
 
 window.addEventListener('scroll',()=>{
